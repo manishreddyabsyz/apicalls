ed864ffb492436e2a72bc72e1dcd37326925fc56
manishreddyabsyz <manish.naini@absyz.com>
2023-10-27 21:30:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+const App = () => {
+  const[users,setUsers]=useState([])
+ const data={fname:"manish",lname:"reddy"}
+  const post=()=>{
+    axios.post("https://jsonplaceholder.typicode.com/users",data)
+  }
 
-function App() {
+const put=()=>{
+  axios.put("https://jsonplaceholder.typicode.com/users/1",data).then(response=>console.log(response))
+}
+const deleteHandler=()=>{
+  axios.delete("https://jsonplaceholder.typicode.com/users/1").then(response=>console.log(response))
+}
+  const getUsers=async()=>{
+    await axios.get("https://jsonplaceholder.typicode.com/users").then(res=>setUsers(res.data)).catch(err=>console.log(err))
+
+  }
+
+  useEffect(()=>{
+    getUsers()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='users-container'>
+      {users.map(user=>{
+        return (
+          <>
+          
+          <h3>{user.name}</h3>
+          </>
+        )
+      })}
+      <button onClick={post}>Post</button>
+      <button onClick={put}>Put</button>
+      <button onClick={deleteHandler}>Delete</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
